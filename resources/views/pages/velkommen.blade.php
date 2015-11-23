f6c274456ca474f4493d589c9429bbd59466611e
Nils Martinussen <nilsma@nma-deb.halsvarden>
2015-11-24 00:14:24 +0100

    post calendar implementation

--- resources/views/pages/velkommen.blade.php
+++ resources/views/pages/velkommen.blade.php
@@ -1,16 +1,17 @@
 @extends('app')
 @section('body-id', 'velkommen')
 @section('content')
-    <div class="col-lg-7">
+<div class="col-lg-7">
         <h2>Main content here</h2>
         <article>
             <p>Some paragraphs happening here</p>
         </article>
     </div>
     <div class="col-lg-5">
-        <h2>Side content here</h2>
-        <aside>
-            <p>some side content here</p>
-        </aside>
+        <h2>Hva skjer på Austegardstoppen</h2>
+        <div>
+            <ul id="eventlist">
+            </ul>
+        </div>
     </div>
 @endsection
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,5 +3,24 @@
     {!!HTML::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css')!!}
     {!! HTML::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js') !!}
     {!! HTML::style('https://fonts.googleapis.com/css?family=Lato:100') !!}
+    {!! HTML::script('js/jquery.min.js') !!}
+    {!! HTML::script('js/g_cal.js') !!}
+
+    <style type="text/css">
+        #eventlist { list-style:none; }
+        #eventlist li { list-style:none; padding:3px; border-bottom:2px solid #eee;}
+        .eventtitle { font-weight:bold; }
+    </style>
+
+    <script type="text/javascript">
+        jQuery(function ($) {
+            $('#eventlist').gCalReader({
+                calendarId:'2gmna9efup7a0nr0l9kdploc10@group.calendar.google.com',
+                apiKey:'AIzaSyAVhU0GdCZQidylxz7whIln82rWtZ4cIDQ',
+                sortDescending: false
+            });
+        });
+    </script>
+
     <title>@yield('title')</title>
 </head>
